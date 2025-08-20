bb970f8601ff13bb5bdc3d670cc5a338243ea859
Gungunbansall <22BCS14702@cuchd.in>
2025-08-20 14:30:40 +0530

    Time: 0 ms (100%), Space: 40.2 MB (80.83%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,10 +1,19 @@
 class Solution {
     public int fib(int n) {
-        if (n <= 1) {
+        if(n<=1){
             return n;
-        } else {
-            return fib(n - 1) + fib(n - 2);
         }
-    }
     
+            int[] fib = new int[n+1];
+            fib[0] = 0;
+                    fib[1] = 1;
+            for(int i =2;i<=n;i++){
+                fib[i] = fib[i-1]+fib[i-2];
+                
+            }
+           
+
+        
+         return fib[n];
     }
+}
