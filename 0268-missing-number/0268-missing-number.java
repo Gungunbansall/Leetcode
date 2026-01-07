9881d698788e2ce7c777322b8bf95151c6a112f3
Gungunbansall <22BCS14702@cuchd.in>
2026-01-07 12:15:04 +0530

    Time: 0 ms (100%), Space: 47.4 MB (50.15%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
+        int actual_sum =0;
         int expected_sum = n*(n+1)/2;
-        int actual_sum=0;
-        for(int i =0;i<nums.length;i++){
-            actual_sum+=nums[i];
+        for(int i : nums){
+            actual_sum += i;
         }
         return expected_sum - actual_sum;
         
-            }
+    }
 }
