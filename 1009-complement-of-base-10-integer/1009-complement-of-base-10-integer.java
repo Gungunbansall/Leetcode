1085cad4b7e3fccb2b176c54d88ef0bc081ace29
Gungunbansall <22BCS14702@cuchd.in>
2026-03-11 15:33:44 +0530

    Time: 0 ms (100%), Space: 42.3 MB (50.09%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        int bits = Integer.toBinaryString(n).length();
+        int mask = (1 << bits) - 1;
+        return n ^ mask;
+    }
+}
